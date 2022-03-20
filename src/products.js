140f5f5873363c2e88b6c69bf839bc3f0e1275ca
David <davidbalyan9@gmail.com>
2022-03-20 23:57:34 +0200

    yea

--- src/products.js
+++ src/products.js
@@ -58,47 +58,27 @@ export const products = [
 					{
 						title: "Orange",
 						price: 1.49,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-pink-lady.png"
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/citrus/orange.png"
 					},
 					{
-						title: "Empire",
+						title: "Tangerine",
 						price: 1.99,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-empire.png"
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/citrus/tangerine.png"
 					},
 					{
-						title: "Fuji",
-						price: 2.99,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-fuji.png"
+						title: "Lime",
+						price: 1.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/citrus/lime.png"
 					},
 					{
-						title: "Gala",
+						title: "Lemon",
 						price: 2.49,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-gala.png"
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/citrus/lemon.png"
 					},
 					{
-						title: "Golden Delicious",
-						price: 1.99,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-golden-delicious.png"
-					},
-					{
-						title: "Red Delicious",
-						price: 2.99,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-red-delicious.png"
-					},
-					{
-						title: "Granny Smith",
-						price: 4.99,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-granny-smith.png"
-					},
-					{
-						title: "Honeycrisp",
-						price: 1.49,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-honeycrisp.png"
-					},
-					{
-						title: "McIntosh",
-						price: 4.99,
-						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/apples/apple-mcIntosh.png"
+						title: "Grapefruit",
+						price: 3.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/citrus/grapefruit.png"
 					}
 				]
 			}
