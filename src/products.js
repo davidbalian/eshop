b9226439bf0cda1d71d7a108a11bcb23d04bf8dc
David <davidbalyan9@gmail.com>
2022-03-21 11:08:23 +0200

    Added berries

--- src/products.js
+++ src/products.js
@@ -81,6 +81,81 @@ export const products = [
 						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/citrus/grapefruit.png"
 					}
 				]
+			},
+			{
+				title: "stone fruit",
+				prods: [
+					{
+						title: "Nectarine",
+						price: 1.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/nectarine.png"
+					},
+					{
+						title: "Apricot",
+						price: 1.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/apricot.png"
+					},
+					{
+						title: "Peach",
+						price: 1.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/peach.png"
+					},
+					{
+						title: "Plum",
+						price: 2.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/plum.png"
+					},
+					{
+						title: "Cherries",
+						price: 1.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/cherry.png"
+					},
+					{
+						title: "Mango",
+						price: 1.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/mango.png"
+					},
+					{
+						title: "Avocado",
+						price: 2.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/stone-fruit/avocado.png"
+					}
+				]
+			},
+			{
+				title: "berries",
+				prods: [
+					{
+						title: "Blueberries",
+						price: 1.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/berries/blueberry.png"
+					},
+					{
+						title: "Raspberries",
+						price: 1.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/berries/raspberry.png"
+					},
+					{
+						title: "Strawberries",
+						price: 1.99,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/berries/strawberry.png"
+					},
+					{
+						title: "Cranberries",
+						price: 2.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/berries/cranberry.png"
+					},
+					{
+						title: "Grapes",
+						price: 1.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/berries/grape.png"
+					},
+					{
+						title: "Banana",
+						price: 1.49,
+						src: "https://cdn.jsdelivr.net/gh/davidbalian/eshop/src/imgs/berries/banana.png"
+					}
+				]
 			}
 		]
 	}
